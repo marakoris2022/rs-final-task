981fcdc2430174efcdf7d3a02a48af32c16f2099
Olga Krukova <krukovaolga1@gmail.com>
2024-05-08 15:08:16 +0300

    feat: implement draft of customers sign-up and login api

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,19 @@ import Registration from './pages/registration/Registration';
 import Notfoundpage from './pages/notfoundpage/Notfoundpage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import { login, signUp, tokenRequest } from './utils/commers-tools-api';
+import { useEffect } from 'react';
 
 function App() {
+  const password = 'Kr1uk3ov5a78$'
+  const email = 'krukovaolga2@gmail.com'
+  const firstName = 'Olga';
+  const lastName = 'Krukova';
+
+  useEffect(() => {
+    /* signUp(email, firstName, lastName, password); */
+  }, [])
+
   return (
     <>
       <Header />
