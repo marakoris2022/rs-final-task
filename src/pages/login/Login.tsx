ff05ab0121a70eb405d8319061c6116a9617a309
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-05-16 12:40:28 +0300

    fix: adaptive styles

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,12 +1,12 @@
 import { RegistrationPrompt } from '../../components/registration-prompt/registration-prompt';
 import { LoginForm } from './login-form/login-form';
-import './login.scss';
+import styles from './login.module.scss';
 
 export default function Login() {
   return (
     <main>
       <section>
-        <h2 style={{ textAlign: 'center', marginBottom: '50px' }}>Login</h2>
+        <h2 className={styles.loginPageTitle}>Login</h2>
         <LoginForm />
         <RegistrationPrompt />
       </section>
