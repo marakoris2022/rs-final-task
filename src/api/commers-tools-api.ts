e48b4516054b6433b0993152131aea0c0c565015
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-05-23 10:58:38 +0300

    feat: comment extra variable and move _redirects settings to the public folder

--- src/api/commers-tools-api.ts
+++ src/api/commers-tools-api.ts
@@ -94,7 +94,7 @@ export async function login(email: string, password: string): Promise<LoginProps
           accessToken: access_token,
         }),
       );
-      const customer = await getCustomerById(customer_id, access_token);
+      // const customer = await getCustomerById(customer_id, access_token); REMOVE OR USE IT !?
     }
     return response.data;
   } catch (error) {
