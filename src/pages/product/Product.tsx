c9fb7411d7352a5a21373afd38cc7dcdc7be3e7c
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-05-23 20:02:46 +0300

    feat: added logic for page loading

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -9,15 +9,18 @@ type ProductData = {
   title: string;
   description: string;
   imageTitle: string;
+  images: Array<string>;
 };
 
 export const Product = () => {
   const navigate = useNavigate();
   const { key } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
   const [productData, setProductData] = useState<ProductData>({
-    title: 'No data',
-    description: 'No data',
-    imageTitle: 'No data',
+    title: '',
+    description: '',
+    imageTitle: '',
+    images: [],
   });
 
   async function renderProductData(productKey: string) {
@@ -28,12 +31,18 @@ export const Product = () => {
       const title = fetchedData.masterData.current.name.en as string;
       const description = fetchedData.masterData.current.description['en-US'] as string;
       const imageTitle = fetchedData.masterData.current.masterVariant.images[0].url as string;
+      const imagesJson = fetchedData.masterData.current.masterVariant.attributes[5].value as string;
+      const images = JSON.parse(imagesJson) as Array<string>;
+
+      console.log('images', images);
 
       setProductData({
         title,
         description,
         imageTitle,
+        images,
       });
+      setIsLoading(false);
     } catch {
       navigate('/NotFoundPage');
     }
@@ -43,14 +52,31 @@ export const Product = () => {
     key && renderProductData(key);
   }, [key]);
 
-  return (
+  return isLoading ? (
     <div className={styles.productWrapper}>
-      <p>Card Data</p>
-      <div>
-        <img src={productData.imageTitle} alt="Game Image" />
+      <p className={styles.loadingPage}>Page is loading...</p>
+    </div>
+  ) : (
+    <div className={styles.productWrapper}>
+      <p className={styles.title}>{productData.title}</p>
+
+      <div className={styles.imageTitleWrapper}>
+        <div className={styles.imageTitleBorder}>
+          <img className={styles.imageTitle} src={productData.imageTitle} alt="Game Image" />
+        </div>
       </div>
-      <p>Product name: {productData.title}</p>
+
       <p>Description: {productData.description}</p>
+
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '15px' }}>
+        {productData.images.map((image) => {
+          return (
+            <div>
+              <img style={{ width: '340px' }} src={image} alt="game image" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
