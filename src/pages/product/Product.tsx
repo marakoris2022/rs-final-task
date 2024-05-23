679e0452c3878b18c458a11fbe39323524190a48
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-05-24 00:18:17 +0300

    feat: added additional product info and video

--- src/pages/product/Product.tsx
+++ src/pages/product/Product.tsx
@@ -1,9 +1,10 @@
 import styles from './product.module.scss';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, json } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { getProductByKey } from '../../api/commers-tools-api';
 import { Carousel } from '../../components/carousel/Carousel';
 import { ModalWindow } from '../../components/modal/ModalWindow';
+import { Button } from '../../components/button/Button';
 
 // fetchedData - is 'any' -> fix it?
 
@@ -13,6 +14,12 @@ type ProductData = {
   imageTitle: string;
   images: Array<string>;
   modalImages: Array<string>;
+  price: number;
+  releaseDate: string;
+  positive: string;
+  userScore: string;
+  categoriesAdd: Array<string>;
+  movie: Array<string>;
 };
 
 export const Product = () => {
@@ -26,6 +33,12 @@ export const Product = () => {
     imageTitle: '',
     images: [],
     modalImages: [],
+    price: 0,
+    releaseDate: '',
+    positive: '',
+    userScore: '',
+    categoriesAdd: [],
+    movie: [],
   });
 
   async function renderProductData(productKey: string) {
@@ -39,6 +52,16 @@ export const Product = () => {
       const imagesJson = fetchedData.masterData.current.masterVariant.attributes[5].value as string;
       const images = JSON.parse(imagesJson) as Array<string>;
       const modalImages = [imageTitle, ...images];
+      const price = fetchedData.masterData.current.masterVariant.prices[0].value.centAmount;
+
+      const releaseDate = fetchedData.masterData.current.masterVariant.attributes[4].value as string;
+      const positive = fetchedData.masterData.current.masterVariant.attributes[7].value as string;
+      const userScore = fetchedData.masterData.current.masterVariant.attributes[0].value as string;
+      const categoriesJson = fetchedData.masterData.current.masterVariant.attributes[2].value as string;
+      const categoriesAdd = JSON.parse(categoriesJson);
+
+      const movieJson = fetchedData.masterData.current.masterVariant.attributes[3].value as string;
+      const movie = JSON.parse(movieJson);
 
       setProductData({
         title,
@@ -46,6 +69,12 @@ export const Product = () => {
         imageTitle,
         images,
         modalImages,
+        price,
+        releaseDate,
+        positive,
+        userScore,
+        categoriesAdd,
+        movie,
       });
       setIsLoading(false);
     } catch {
@@ -74,10 +103,48 @@ export const Product = () => {
           </div>
         </div>
       </div>
+      <div className={styles.buyWrapper}>
+        <p className={styles.buyPromo}>Buy it now! </p>
+        <Button style={styles.buyBtn} title={`${productData.price / 100} USD`} />
+      </div>
 
       <div className={styles.carouselWrapper}>
         <Carousel images={productData.images} />
       </div>
+
+      <div>
+        <h2 className={styles.addTitle}>Additional game data:</h2>
+        <ul className={styles.addWrapper}>
+          <li>Game released: {productData.releaseDate}</li>
+          <li>Positive: {productData.positive}</li>
+          <li>User score: {productData.userScore}</li>
+          {productData.categoriesAdd.length && (
+            <li>
+              Steam categories:
+              <ul>
+                {productData.categoriesAdd.map((item, index) => {
+                  return <li key={`${index}_steam_cat`}>{item}</li>;
+                })}
+              </ul>
+            </li>
+          )}
+        </ul>
+      </div>
+
+      {productData.movie.length > 0 && (
+        <div>
+          <h2 className={styles.addTitle}>Game Movie</h2>
+          {productData.movie.map((video, index) => {
+            return (
+              <div key={`${index}_video`} className={styles.videoWrapper}>
+                <video className={styles.video} controls>
+                  <source src={video} />
+                </video>
+              </div>
+            );
+          })}
+        </div>
+      )}
 
       {isModal && (
         <ModalWindow
