aa52a6e335b946b9322f5f5a8d6aa24b5ec30efd
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-05-30 12:30:04 +0300

    feat: styles

--- src/components/user-addresses/UserAddresses.tsx
+++ src/components/user-addresses/UserAddresses.tsx
@@ -82,155 +82,155 @@ export const UserAddresses = () => {
     setIsLoading(() => false);
   };
 
-  console.log(customer);
-
   return (
-    <div>
+    <>
       <Button
         style={styles.newAddressBtn}
         title="Add new address"
         type="button"
         onClick={() => navigate('/profile/addresses/add-address')}
       />
-      <div className={styles.shippingAddressesContainer}>
-        <h3 className={styles.addressesTitle}>Shipping Addresses</h3>
+      <div className={styles.addressesWrapper}>
+        <div className={styles.shippingAddressesContainer}>
+          <h3 className={styles.addressesTitle}>Shipping Addresses</h3>
 
-        {shippingAddresses?.map((currAddress) => {
-          const isDefaultAddress = customer?.defaultShippingAddressId === currAddress.id;
+          {shippingAddresses?.map((currAddress) => {
+            const isDefaultAddress = customer?.defaultShippingAddressId === currAddress.id;
 
-          return (
-            <div className={styles.addressContainer} key={currAddress.id}>
-              <div className={styles.addressWrapper}>
-                <div className={styles.addressItemContainer}>
-                  <h4>First name:</h4>
-                  <div className={styles.addressValue}>{currAddress.firstName}</div>
+            return (
+              <div className={styles.addressContainer} key={currAddress.id}>
+                <div className={styles.addressWrapper}>
+                  <div className={styles.addressItemContainer}>
+                    <h4>First name:</h4>
+                    <div className={styles.addressValue}>{currAddress.firstName}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Last name:</h4>
+                    <div className={styles.addressValue}>{currAddress.lastName}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Country:</h4>
+                    <div className={styles.addressValue}>{CountryCodes[currAddress.country]}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Postal Code:</h4>
+                    <div className={styles.addressValue}>{currAddress.postalCode}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>City:</h4>
+                    <div className={styles.addressValue}>{currAddress.city}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Street:</h4>
+                    <div className={styles.addressValue}>{currAddress.streetName}</div>
+                  </div>
                 </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Last name:</h4>
-                  <div className={styles.addressValue}>{currAddress.lastName}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Country:</h4>
-                  <div className={styles.addressValue}>{CountryCodes[currAddress.country]}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Postal Code:</h4>
-                  <div className={styles.addressValue}>{currAddress.postalCode}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>City:</h4>
-                  <div className={styles.addressValue}>{currAddress.city}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Street:</h4>
-                  <div className={styles.addressValue}>{currAddress.streetName}</div>
+                <div className={styles.addressManageContainer}>
+                  {isDefaultAddress && <div className={styles.addressDefault}>Default shipping</div>}
+                  <div className={styles.addressManageBtnsContainer}>
+                    {!isDefaultAddress && (
+                      <Button
+                        style={styles.setDefaultAddressBtn}
+                        title="Set as default"
+                        type="button"
+                        onClick={async () => {
+                          await handleSetDefaultAddress(currAddress!.id, DefaultAddressTypes.SHIPPING);
+                        }}
+                        disabled={isLoading}
+                      />
+                    )}
+                    {!isDefaultAddress && (
+                      <Button
+                        style={styles.setDefaultAddressBtn}
+                        title="Set as billing"
+                        type="button"
+                        onClick={() => {
+                          handleChangeAddressType(currAddress.id, RemoveAddressTypes.SHIPPING, AddressTypes.BILLING);
+                        }}
+                        disabled={isLoading}
+                      />
+                    )}
+                  </div>
+                  <div className={styles.addressIconsContainer}>
+                    <FaEdit />
+                    <FaTrash onClick={() => !isLoading && openDeleteModal(currAddress.id)} />
+                  </div>
                 </div>
               </div>
-              <div className={styles.addressManageContainer}>
-                {isDefaultAddress && <div className={styles.addressDefault}>Default shipping</div>}
-                <div className={styles.addressManageBtnsContainer}>
-                  {!isDefaultAddress && (
-                    <Button
-                      style={styles.setDefaultAddressBtn}
-                      title="Set as default"
-                      type="button"
-                      onClick={async () => {
-                        await handleSetDefaultAddress(currAddress!.id, DefaultAddressTypes.SHIPPING);
-                      }}
-                      disabled={isLoading}
-                    />
-                  )}
-                  {!isDefaultAddress && (
-                    <Button
-                      style={styles.setDefaultAddressBtn}
-                      title="Set as billing"
-                      type="button"
-                      onClick={() => {
-                        handleChangeAddressType(currAddress.id, RemoveAddressTypes.SHIPPING, AddressTypes.BILLING);
-                      }}
-                      disabled={isLoading}
-                    />
-                  )}
+            );
+          })}
+        </div>
+
+        <div className={styles.shippingAddressesContainer}>
+          <h3 className={styles.addressesTitle}>Billing Addresses</h3>
+
+          {billingAddresses?.map((currAddress) => {
+            const isDefaultAddress = customer?.defaultBillingAddressId === currAddress.id;
+
+            return (
+              <div className={styles.addressContainer} key={currAddress.id}>
+                <div className={styles.addressWrapper}>
+                  <div className={styles.addressItemContainer}>
+                    <h4>First name:</h4>
+                    <div className={styles.addressValue}>{currAddress.firstName}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Last name:</h4>
+                    <div className={styles.addressValue}>{currAddress.lastName}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Country:</h4>
+                    <div className={styles.addressValue}>{CountryCodes[currAddress.country]}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Postal Code:</h4>
+                    <div className={styles.addressValue}>{currAddress.postalCode}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>City:</h4>
+                    <div className={styles.addressValue}>{currAddress.city}</div>
+                  </div>
+                  <div className={styles.addressItemContainer}>
+                    <h4>Street:</h4>
+                    <div className={styles.addressValue}>{currAddress.streetName}</div>
+                  </div>
                 </div>
-                <div className={styles.addressIconsContainer}>
-                  <FaEdit />
-                  <FaTrash onClick={() => !isLoading && openDeleteModal(currAddress.id)} />
+                <div className={styles.addressManageContainer}>
+                  {isDefaultAddress && <div className={styles.addressDefault}>Default billing</div>}
+                  <div className={styles.addressManageBtnsContainer}>
+                    {!isDefaultAddress && (
+                      <Button
+                        style={styles.setDefaultAddressBtn}
+                        title="Set as default"
+                        type="button"
+                        onClick={async () => {
+                          await handleSetDefaultAddress(currAddress!.id, DefaultAddressTypes.BILLING);
+                        }}
+                        disabled={isLoading}
+                      />
+                    )}
+                    {!isDefaultAddress && (
+                      <Button
+                        style={styles.setDefaultAddressBtn}
+                        title="Set as shipping"
+                        type="button"
+                        onClick={() => {
+                          handleChangeAddressType(currAddress.id, RemoveAddressTypes.BILLING, AddressTypes.SHIPPING);
+                        }}
+                        disabled={isLoading}
+                      />
+                    )}
+                  </div>
+
+                  <div className={styles.addressIconsContainer}>
+                    <FaEdit />
+                    <FaTrash onClick={() => !isLoading && openDeleteModal(currAddress.id)} />
+                  </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-
-      <div className={styles.shippingAddressesContainer}>
-        <h3 className={styles.addressesTitle}>Billing Addresses</h3>
-
-        {billingAddresses?.map((currAddress) => {
-          const isDefaultAddress = customer?.defaultBillingAddressId === currAddress.id;
-
-          return (
-            <div className={styles.addressContainer} key={currAddress.id}>
-              <div className={styles.addressWrapper}>
-                <div className={styles.addressItemContainer}>
-                  <h4>First name:</h4>
-                  <div className={styles.addressValue}>{currAddress.firstName}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Last name:</h4>
-                  <div className={styles.addressValue}>{currAddress.lastName}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Country:</h4>
-                  <div className={styles.addressValue}>{CountryCodes[currAddress.country]}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Postal Code:</h4>
-                  <div className={styles.addressValue}>{currAddress.postalCode}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>City:</h4>
-                  <div className={styles.addressValue}>{currAddress.city}</div>
-                </div>
-                <div className={styles.addressItemContainer}>
-                  <h4>Street:</h4>
-                  <div className={styles.addressValue}>{currAddress.streetName}</div>
-                </div>
-              </div>
-              <div className={styles.addressManageContainer}>
-                {isDefaultAddress && <div className={styles.addressDefault}>Default billing</div>}
-                <div className={styles.addressManageBtnsContainer}>
-                  {!isDefaultAddress && (
-                    <Button
-                      style={styles.setDefaultAddressBtn}
-                      title="Set as default"
-                      type="button"
-                      onClick={async () => {
-                        await handleSetDefaultAddress(currAddress!.id, DefaultAddressTypes.BILLING);
-                      }}
-                      disabled={isLoading}
-                    />
-                  )}
-                  {!isDefaultAddress && (
-                    <Button
-                      style={styles.setDefaultAddressBtn}
-                      title="Set as shipping"
-                      type="button"
-                      onClick={() => {
-                        handleChangeAddressType(currAddress.id, RemoveAddressTypes.BILLING, AddressTypes.SHIPPING);
-                      }}
-                      disabled={isLoading}
-                    />
-                  )}
-                </div>
-
-                <div className={styles.addressIconsContainer}>
-                  <FaEdit />
-                  <FaTrash onClick={() => !isLoading && openDeleteModal(currAddress.id)} />
-                </div>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
 
       {showModal && (
@@ -241,6 +241,6 @@ export const UserAddresses = () => {
           onConfirm={() => handleDeleteAddress()}
         />
       )}
-    </div>
+    </>
   );
 };
