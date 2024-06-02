762d88e58dbcbcdee7cb11dec1bccf9f3de444f7
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-06-02 19:24:08 +0300

    init: project logo to navigate component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,17 @@
 import styles from './header.module.scss';
 import { Navigation } from '../navigation/Navigation';
+import { useNavigate } from 'react-router-dom';
+import logoImg from '/website_logo.png';
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <header className={styles.header}>
-      <h2>Header</h2>
+      <div onClick={() => navigate('/')} className={styles.logoWrapper}>
+        <img className={styles.logoImg} src={logoImg} alt="Logo" />
+        <span className={styles.logoText}>RSTeam Games Store</span>
+      </div>
       <Navigation />
     </header>
   );
