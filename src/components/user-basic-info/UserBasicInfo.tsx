c001bfb3e5ce5a949eb1d3d1ad334d67b886d34c
Alexander Pakhomov <feikomilcezzz2@gmail.com>
2024-06-04 17:05:24 +0300

    fix: button change address

--- src/components/user-basic-info/UserBasicInfo.tsx
+++ src/components/user-basic-info/UserBasicInfo.tsx
@@ -134,83 +134,78 @@ export const UserBasicInfo = () => {
           <h4>Your Date of Birth:</h4>
           <p className={styles.basicInfoItem}>{convertDateToReadableFormat(customer?.dateOfBirth)}</p>
         </div>
+        <FaEdit className={styles.modifyIco} onClick={() => setIsOpenModify((prev) => !prev)} />
       </div>
       <form className={styles.userForm} onSubmit={formik.handleSubmit}>
-        <div className={styles.basicInfoContainer}>
-          <h3>Change Your Personal Information</h3>
-          <hr />
-
-          <FaEdit className={styles.modifyIco} onClick={() => setIsOpenModify((prev) => !prev)} />
-          {isOpenModify && (
-            <ModalWindow
-              children={
-                <div className={`${styles.basicInfoContainer} ${styles.basicInfoContainerWithoutMargin}`}>
-                  <FormField
-                    stylesError={styles.profileFormError}
-                    stylesInput={styles.profileFormInput}
-                    stylesInputWrapper={styles.labelInputContainer}
-                    formik={formik}
-                    labelText="Email:"
-                    placeholder="example@gmail.com"
-                    id="email"
-                    name="email"
-                    type="text"
-                    autoComplete="email"
-                  ></FormField>
-                  <FormField
-                    stylesError={styles.profileFormError}
-                    stylesInput={styles.profileFormInput}
-                    stylesInputWrapper={styles.labelInputContainer}
-                    formik={formik}
-                    labelText="First Name:"
-                    placeholder="First name"
-                    id="firstName"
-                    name="firstName"
-                    type="text"
-                  ></FormField>
-                  <FormField
-                    stylesError={styles.profileFormError}
-                    stylesInput={styles.profileFormInput}
-                    stylesInputWrapper={styles.labelInputContainer}
-                    formik={formik}
-                    labelText="Last Name:"
-                    placeholder="Last name"
-                    id="lastName"
-                    name="lastName"
-                    type="text"
-                  ></FormField>
-                  <FormField
-                    stylesError={styles.profileFormError}
-                    stylesInput={styles.profileFormInput}
-                    stylesInputWrapper={styles.labelInputContainer}
-                    formik={formik}
-                    labelText="Date of Birth:"
-                    id="dateOfBirth"
-                    name="dateOfBirth"
-                    type="date"
-                    max="2010-01-01"
-                  ></FormField>
-                  <Button
-                    style={styles.profileFormBtn}
-                    title="Save"
-                    type="submit"
-                    disabled={!formik.isValid || formik.isSubmitting || !isModified}
-                  />
-                </div>
-              }
-              onClose={() => setIsOpenModify(false)}
-            />
-          )}
-          {message && (
-            <ModalWindow
-              message={message}
-              onClose={() => {
-                setMessage(() => '');
-                setIsOpenModify(false);
-              }}
-            />
-          )}
-        </div>
+        {isOpenModify && (
+          <ModalWindow
+            children={
+              <div className={`${styles.basicInfoContainer} ${styles.basicInfoContainerWithoutMargin}`}>
+                <FormField
+                  stylesError={styles.profileFormError}
+                  stylesInput={styles.profileFormInput}
+                  stylesInputWrapper={styles.labelInputContainer}
+                  formik={formik}
+                  labelText="Email:"
+                  placeholder="example@gmail.com"
+                  id="email"
+                  name="email"
+                  type="text"
+                  autoComplete="email"
+                ></FormField>
+                <FormField
+                  stylesError={styles.profileFormError}
+                  stylesInput={styles.profileFormInput}
+                  stylesInputWrapper={styles.labelInputContainer}
+                  formik={formik}
+                  labelText="First Name:"
+                  placeholder="First name"
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                ></FormField>
+                <FormField
+                  stylesError={styles.profileFormError}
+                  stylesInput={styles.profileFormInput}
+                  stylesInputWrapper={styles.labelInputContainer}
+                  formik={formik}
+                  labelText="Last Name:"
+                  placeholder="Last name"
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                ></FormField>
+                <FormField
+                  stylesError={styles.profileFormError}
+                  stylesInput={styles.profileFormInput}
+                  stylesInputWrapper={styles.labelInputContainer}
+                  formik={formik}
+                  labelText="Date of Birth:"
+                  id="dateOfBirth"
+                  name="dateOfBirth"
+                  type="date"
+                  max="2010-01-01"
+                ></FormField>
+                <Button
+                  style={styles.profileFormBtn}
+                  title="Save"
+                  type="submit"
+                  disabled={!formik.isValid || formik.isSubmitting || !isModified}
+                />
+              </div>
+            }
+            onClose={() => setIsOpenModify(false)}
+          />
+        )}
+        {message && (
+          <ModalWindow
+            message={message}
+            onClose={() => {
+              setMessage(() => '');
+              setIsOpenModify(false);
+            }}
+          />
+        )}
       </form>
     </div>
   );
