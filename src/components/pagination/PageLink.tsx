1476ef090158fd2ae11595d46a31a1607da1c962
Olga Krukova <krukovaolga1@gmail.com>
2024-06-11 09:41:17 +0300

    fix: add styles for active and disabled

--- src/components/pagination/PageLink.tsx
+++ src/components/pagination/PageLink.tsx
@@ -5,7 +5,7 @@ import styles from './pageLink.module.scss';
 export type Props = HTMLProps<HTMLAnchorElement> & { active?: boolean };
 
 export default function PageLink({ className, active, disabled, children, ...otherProps }: Props) {
-  const customClassName = cn(styles.pageLink, className, { active, disabled });
+  const customClassName = cn(styles.pageLink, className, { [styles.active]: active, [styles.disabled]: disabled });
 
   if (disabled) {
     return <span className={customClassName}>{children}</span>;
