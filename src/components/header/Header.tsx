b6ff5e5c2d2377f64a8fd90e31457722e40d6819
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-06-20 22:29:36 +0300

    added Enum to the Header home path

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,13 +2,14 @@ import styles from './header.module.scss';
 import { Navigation } from '../navigation/Navigation';
 import { useNavigate } from 'react-router-dom';
 import logoImg from '/website_logo.png';
+import { Path } from '../../interfaces/enum';
 
 export const Header = () => {
   const navigate = useNavigate();
 
   return (
     <header className={styles.header}>
-      <div onClick={() => navigate('/')} className={styles.logoWrapper}>
+      <div onClick={() => navigate(Path.Home)} className={styles.logoWrapper}>
         <img className={styles.logoImg} src={logoImg} alt="Logo" />
         <span className={styles.logoText}>RSTeam Games Store</span>
       </div>
